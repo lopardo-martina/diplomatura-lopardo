0637a0c73d246e9a004f110306b4c99a696875a0
lopardo-martina <lopardo.martina04@gmail.com>
2026-01-08 18:06:57 -0300

    Cambios en los estaticos del header  y footer

--- m4u1/src/components/header.js
+++ m4u1/src/components/header.js
@@ -1,7 +1,15 @@
 export default function Header() {
     return (
-        <header>
+        <header style= {{ backgroundColor: '#fff', color: '#637eeaff', padding: '10px'}}>
             <h1>Mi primer Proyecto en Next.js</h1>
+
+            <nav>
+                <ul>
+                    <li>Inicio</li>
+                    <li>Nosotros</li>
+                    <li>Contacto</li>
+                </ul>
+            </nav>
         </header>
     );
 }
