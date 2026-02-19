b6f3bb93c126deb4b54a3662f38b586b74606ddd
lopardo-martina <lopardo.martina04@gmail.com>
2026-02-19 11:28:47 -0300

    actualizacion m4u2

--- m4u2/src/components/destinoItem.js
+++ m4u2/src/components/destinoItem.js
@@ -1,14 +1,14 @@
-export default async function DestinoItem(){
+export default async function DestinoItem({imagen, nombre, tipo_destino, texto, tiempo, precio}){
     return (
         <div className="destino-card card">
-            <img src="/img/ecoparque.jpg" className="destino-img" alt="Ecoparque Buenos Aires" />
+            <img src={imagen} className="destino-img" alt={nombre} />
             <div className="card-body destino-content">
-                <span className="tipo-destino">Parque</span>
-                <h3>Ecoparque</h3>
-                <p className="card-text">Espacio de conservación y educación ambiental. Antiguo zoológico transformado en centro ecológico.</p>
+                <span className="tipo-destino">{tipo_destino}</span>
+                <h3>{nombre}</h3>
+                <p className="card-text">{texto}</p>
                 <div className="destino-info">
-                    <span className="info-item"><i className="fa-regular fa-clock"></i> 2-3 horas</span>
-                    <span className="info-item"><i className="fa-solid fa-dollar-sign"></i> Gratis</span>
+                    <span className="info-item"><i className="fa-regular fa-clock"></i> {tiempo} horas</span>
+                    <span className="info-item"><i className="fa-solid fa-dollar-sign"></i> $ {precio}</span>
                 </div>
             </div>
         </div>
