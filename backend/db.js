7b416f52a28bb0f19e6e4ad992578f99f96bf0bb
lopardo-martina <lopardo.martina04@gmail.com>
2026-02-21 01:16:56 -0300

    se cambio mysql

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Unicornio123.",
-  database: "bairways"
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB_NAME,
+  port: process.env.MYSQL_PORT
 });
 
 connection.connect((err) => {
